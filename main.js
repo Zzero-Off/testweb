957199525246f06752fab6eb17258bfc4c63845d
Zzero-Off <michaela.kozlikova@seznam.cz>
2025-12-05 10:37:23 +0100

    wip

---
+++ main.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById("mode-toggle");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  toggle.checked = true;
+}
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+  }
+});
