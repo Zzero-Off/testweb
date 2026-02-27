72d2ffdc2aaa59d078bc07b430db04866f882f01
Zzero-Off <michaela.kozlikova@seznam.cz>
2026-02-27 11:58:37 +0100

    Related to #36

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+	const galleryImages = document.querySelectorAll(".js-gallery-image");
 
-    const galleryImages = document.querySelectorAll(".js-gallery-image");
+	for (const img of galleryImages) {
+		img.addEventListener("click", () => {
+			alert("^_^");
+		});
+	}
 
-    for (const img of galleryImages) {
-        img.addEventListener("click", () => {
-            alert("^_^");
-        });
-    }
-
+	/** @type {NodeListOf<HTMLElement>} */
 	const likeButtons = document.querySelectorAll(".js-like-button");
 
 	for (const btn of likeButtons) {
+		/** @param {Event} e */
 		btn.addEventListener("click", (e) => {
 			e.preventDefault();
 
@@ -20,27 +21,28 @@ document.addEventListener("DOMContentLoaded", () => {
 			if (isNaN(count)) {
 				count = 0;
 			}
-			
-			count ++;
+
+			count++;
 			countSpan.innerText = count;
 		});
 	}
+	/**
+	 * Handles website visit counter using cookies.
+	 * Increments the visit count on each page load.
+	 */
+	const totalCountElement = document.querySelector(".js-total-count");
 
-		const totalCountElement = document.querySelector(".js-total-count");
-		
-		let cookiesArray = document.cookie.split(";");
-		let visitsCookie = cookiesArray.find(cookie => cookie.startsWith("visits"));
-		let numberOfVisits;
-				
-			if (visitsCookie) {
-				numberOfVisits = Number(visitsCookie.split("=")[1]) + 1;
-			} else {
-				numberOfVisits = 1;
-			}
+	let cookiesArray = document.cookie.split(";");
+	let visitsCookie = cookiesArray.find((cookie) => cookie.startsWith("visits"));
+	let numberOfVisits;
 
-			document.cookie = "visit" + numberOfVisits + "; path=/";
+	if (visitsCookie) {
+		numberOfVisits = Number(visitsCookie.split("=")[1]) + 1;
+	} else {
+		numberOfVisits = 1;
+	}
 
-			totalCountElement.textContent = numberOfVisits;
+	document.cookie = "visit" + numberOfVisits + "; path=/";
 
-	
+	totalCountElement.textContent = numberOfVisits;
 });
